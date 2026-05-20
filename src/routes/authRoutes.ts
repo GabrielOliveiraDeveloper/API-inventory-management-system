9c2568770a6cddbfe6824280979949d47b3e5d6a
Gabriel-developments <salvadiploma5@gmail.com>
2026-05-20 15:37:47 -0300

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,9 +3,11 @@ import LoginController from "../controllers/auth/LoginController.js";
 import RegisterController from "../controllers/auth/RegisterController.js";
 import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 import requireRole from '../middlewares/Rolemiddleware.js';
+import { authLimiter } from '../middlewares/RateLimiter.js';
+
 const authRoutes = express.Router();
 
-authRoutes.post("/login", LoginController);
+authRoutes.post("/login", authLimiter, LoginController);
 authRoutes.post("/register", AuthMiddleware, requireRole('admin'), RegisterController);
 
 export default authRoutes;
