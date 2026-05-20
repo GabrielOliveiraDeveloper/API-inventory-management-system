0d0148dfb6210536a3a0a50aad74b64aca86b8a7
Gabriel-developments <salvadiploma5@gmail.com>
2026-05-20 15:09:55 -0300

    Update productsRoutes.ts

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,12 +1,13 @@
 import * as ProductsController from "../controllers/products/ProductsController.js";
 import express from "express";
 import AuthMiddleware from "../middlewares/AuthMiddleware.js";
+import requireRole from '../middlewares/Rolemiddleware.js';
 
 const productsRoutes = express.Router();
 
-productsRoutes.post("/create", AuthMiddleware, ProductsController.CreateProduct);
+productsRoutes.post("/create", AuthMiddleware, requireRole('admin'), ProductsController.CreateProduct);
 productsRoutes.get("/list", AuthMiddleware, ProductsController.GetProducts);
-productsRoutes.put("/update/:id", AuthMiddleware, ProductsController.UpdateProduct);
-productsRoutes.delete("/delete/:id", AuthMiddleware, ProductsController.RemoveProduct);
+productsRoutes.put("/update/:id", AuthMiddleware, requireRole('admin'), ProductsController.UpdateProduct);
+productsRoutes.delete("/delete/:id", AuthMiddleware, requireRole('admin'), ProductsController.RemoveProduct);
 
 export default productsRoutes;
